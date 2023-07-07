b5f200d82c8ea3d4a050f194c56ad29ab33ac885
itguy19 <andras.ts04@gmail.com>
2023-07-07 10:18:25 +0200

    The wifi scanning works. (Ugly method). Maybe fix later.

--- app/src/main/java/ch/zli/gainthewifi/service/WifiService.java
+++ app/src/main/java/ch/zli/gainthewifi/service/WifiService.java
@@ -1,6 +1,10 @@
 package ch.zli.gainthewifi.service;
 
-import android.Manifest;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.Manifest.permission.ACCESS_WIFI_STATE;
+
+import static androidx.core.content.ContentProviderCompat.requireContext;
+
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -13,83 +17,27 @@ import android.os.Binder;
 import android.os.IBinder;
 
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import ch.zli.gainthewifi.modal.NetworkItem;
+import ch.zli.gainthewifi.view.MainActivity;
 
 public class WifiService extends Service {
     private final IBinder binder = new WifiBinder();
     private WifiManager wifiManager;
+
     public class WifiBinder extends Binder {
-        WifiService getService() {
+        public WifiService getService() {
             return WifiService.this;
         }
-    }
-
-    public void startScan() {
-        wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
-        context.registerReceiver(wifiScanReceiver, intentFilter);
-
-        // Enable WIFI scan
-        if (!wifiManager.isWifiEnabled()) {
-            wifiManager.setWifiEnabled(true);
-        }
-
-        boolean success = wifiManager.startScan();
-        if (!success) {
-            scanFailure();
-        }
-    }
-
-    public void stopScanning() {
-
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         return binder;
     }
-
-    public ArrayList<NetworkItem> getNetworkList() {
-        return null;
-    }
-
-    private Context context;
-
-    BroadcastReceiver wifiScanReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context c, Intent intent) {
-            boolean success = intent.getBooleanExtra(
-                    WifiManager.EXTRA_RESULTS_UPDATED, false);
-            if (success) {
-                scanSuccess();
-            } else {
-                scanFailure();
-            }
-        }
-    };
-
-    private void scanSuccess() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_WIFI_STATE) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        List<ScanResult> results = wifiManager.getScanResults();
-        for (ScanResult r: results) {
-            System.out.println(r);
-        }
-    }
-
-    private void scanFailure() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_WIFI_STATE) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        List<ScanResult> results = wifiManager.getScanResults();
-    }
-
 
 }
--- app/src/main/java/ch/zli/gainthewifi/view/MainActivity.java
+++ app/src/main/java/ch/zli/gainthewifi/view/MainActivity.java
@@ -3,13 +3,26 @@ package ch.zli.gainthewifi.view;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageButton;
@@ -19,18 +32,38 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.Manifest.permission.ACCESS_NETWORK_STATE;
+import static android.Manifest.permission.ACCESS_WIFI_STATE;
+
+import static androidx.core.content.ContentProviderCompat.requireContext;
+
 import ch.zli.gainthewifi.R;
 import ch.zli.gainthewifi.modal.NetworkItem;
+import ch.zli.gainthewifi.service.DbService;
+import ch.zli.gainthewifi.service.WifiService;
 
 public class MainActivity extends AppCompatActivity {
+
     private ImageButton wifiScanButton;
     private TextView statusTextView;
+    private RecyclerView recyclerView;
     private boolean scanModeOn = false;
+    private boolean isWifiServiceBound;
+    private WifiManager wifiManager;
+    private List<NetworkItem> networkItems;
+    private WifiService wifiService;
+    private DbService dbService;
+
+    public static MainActivity mainActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        // TODO
+        mainActivity = this;
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -41,48 +74,73 @@ public class MainActivity extends AppCompatActivity {
         wifiScanButton = (ImageButton) findViewById(R.id.imageButton);
         setupClickListener();
 
-
-
-        List<NetworkItem> networkItems = new ArrayList<>();
-        networkItems.add(new NetworkItem("DONG-WON UPC89893", "1d-b9-a5-57-c7-d9", 6, "[WPA-TSK-]", -46, R.drawable.wifi_on));
-        networkItems.add(new NetworkItem("DONG-WON UPC89893", "1d-b9-a5-57-c7-d9", 6, "[WPA-TSK-]", -46, R.drawable.wifi_on));
-        networkItems.add(new NetworkItem("DONG-WON UPC89893", "1d-b9-a5-57-c7-d9", 6, "[WPA-TSK-]", -46, R.drawable.wifi_on));
-        networkItems.add(new NetworkItem("DONG-WON UPC89893", "1d-b9-a5-57-c7-d9", 6, "[WPA-TSK-]", -46, R.drawable.wifi_on));
-        networkItems.add(new NetworkItem("DONG-WON UPC89893", "1d-b9-a5-57-c7-d9", 6, "[WPA-TSK-]", -46, R.drawable.wifi_on));
-        networkItems.add(new NetworkItem("DONG-WON UPC89893", "1d-b9-a5-57-c7-d9", 6, "[WPA-TSK-]", -46, R.drawable.wifi_on));
-        networkItems.add(new NetworkItem("DONG-WON UPC89893", "1d-b9-a5-57-c7-d9", 6, "[WPA-TSK-]", -46, R.drawable.wifi_on));
-        networkItems.add(new NetworkItem("DONG-WON UPC89893", "1d-b9-a5-57-c7-d9", 6, "[WPA-TSK-]", -46, R.drawable.wifi_on));
-        networkItems.add(new NetworkItem("DONG-WON UPC89893", "1d-b9-a5-57-c7-d9", 6, "[WPA-TSK-]", -46, R.drawable.wifi_on));
-        networkItems.add(new NetworkItem("DONG-WON UPC89893", "1d-b9-a5-57-c7-d9", 6, "[WPA-TSK-]", -46, R.drawable.wifi_on));
-        networkItems.add(new NetworkItem("DONG-WON UPC89893", "1d-b9-a5-57-c7-d9", 6, "[WPA-TSK-]", -46, R.drawable.wifi_on));
-        networkItems.add(new NetworkItem("DONG-WON UPC89893", "1d-b9-a5-57-c7-d9", 6, "[WPA-TSK-]", -46, R.drawable.wifi_on));
-        networkItems.add(new NetworkItem("DONG-WON UPC89893", "1d-b9-a5-57-c7-d9", 6, "[WPA-TSK-]", -46, R.drawable.wifi_on));
-        networkItems.add(new NetworkItem("DONG-WON UPC89893", "1d-b9-a5-57-c7-d9", 6, "[WPA-TSK-]", -46, R.drawable.wifi_on));
-
-        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        networkItems = new ArrayList<>();
+        recyclerView = findViewById(R.id.recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        checkPermissions();
+    }
+
+    private void checkPermissions() {
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}, 0);
+        }
+    }
+
+    /**************************************WIFI SCANNER**********************************************/
+
+    public void scanWifiList() {
+        wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        registerReceiver(wifiScanReceiver, intentFilter);
+
+        // Enable WIFI scan
+        if (!wifiManager.isWifiEnabled()) {
+            wifiManager.setWifiEnabled(true);
+        }
+
+        boolean success = wifiManager.startScan();
+        if (!success) {
+            scanResults();
+        } else {
+            scanResults();
+        }
+    }
+
+    private void scanResults() {
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        List<ScanResult> results = wifiManager.getScanResults();
+        networkItems.clear();
+        for (ScanResult r: results) {
+            int channel = convertFrequencyToChannel(r.frequency);
+            networkItems.add(new NetworkItem(r.SSID, r.BSSID, channel, r.capabilities, r.level, R.drawable.wifi_on));
+        }
         recyclerView.setAdapter(new ViewAdapter(getApplicationContext(), networkItems));
-
+    }
+    // Generated by ChatGPT
+    private int convertFrequencyToChannel(int frequency) {
+        if (frequency >= 2412 && frequency <= 2484) {
+            // 2.4 GHz band
+            return (frequency - 2412) / 5 + 1;
+        } else if (frequency >= 5180 && frequency <= 5825) {
+            // 5 GHz band
+            return (frequency - 5180) / 5 + 36;
+        } else {
+            // Invalid frequency or channel not found
+            return -1;
+        }
     }
 
-    private void setupClickListener() {
-        wifiScanButton.setOnClickListener(v -> {
-            if (scanModeOn) {
-                wifiScanButton.setImageResource(R.drawable.wifi_off);
-                setStatusMessage("stopped", ResourcesCompat.getColor(getResources(), R.color.red, null));
-            } else {
-                wifiScanButton.setImageResource(R.drawable.wifi_on);
-                setStatusMessage("running", ResourcesCompat.getColor(getResources(), R.color.green, null));
-            }
-            scanModeOn = !scanModeOn;
-        });
-    }
+    BroadcastReceiver wifiScanReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context c, Intent intent) {}
+    };
 
-    @SuppressLint("SetTextI18n")
-    private void setStatusMessage(String message, int color) {
-        statusTextView.setText("Status:\n" + message);
-        statusTextView.setTextColor(color);
-    }
+    /**************************************WIFI SCANNER END****************************************/
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -90,7 +148,34 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent bindBMIServiceIntent = new Intent(this, WifiService.class);
+        bindService(bindBMIServiceIntent, connection, Context.BIND_AUTO_CREATE);
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unbindService(connection);
+        isWifiServiceBound = false;
+    }
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            WifiService.WifiBinder binder = (WifiService.WifiBinder)  iBinder;
+            wifiService = binder.getService();
+            isWifiServiceBound = true;
+            // TODO
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            isWifiServiceBound = false;
+        }
+    };
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         String message = "Nothing clicked";
@@ -103,5 +188,25 @@ public class MainActivity extends AppCompatActivity {
         }
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setupClickListener() {
+        wifiScanButton.setOnClickListener(v -> {
+            if (scanModeOn) {
+                wifiScanButton.setImageResource(R.drawable.wifi_off);
+                setStatusMessage("stopped", ResourcesCompat.getColor(getResources(), R.color.red, null));
+            } else {
+                wifiScanButton.setImageResource(R.drawable.wifi_on);
+                setStatusMessage("running", ResourcesCompat.getColor(getResources(), R.color.green, null));
+                scanWifiList();
+            }
+            scanModeOn = !scanModeOn;
+        });
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void setStatusMessage(String message, int color) {
+        statusTextView.setText("Status:\n" + message);
+        statusTextView.setTextColor(color);
     }
 }
